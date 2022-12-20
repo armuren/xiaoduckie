8aa4653528d1c71acc33846d7f69dd4cd267c30d
Sameh Mohamed <sameh4@gmx.com>
2022-12-20 13:55:22 -0600

    adding sensors package

---
+++ src/sensors/include/wheel_encoder/wheel_encoder.h
@@ -0,0 +1,16 @@
+#pragma once
+
+namespace xiaoduckie {
+namespace sensors {
+
+/*!
+ * @brief This class manages the WheelEncoder sensor connected to the GPIO
+ * @ingroup Sensors
+ */
+class WheelEncoder {
+  public:
+  private:
+};
+
+} // namespace sensors
+} // namespace xiaoduckie
